d7dbbda0ebfce3c3e3120a781c4273aaecd87f8e
alzbetahladikova <beta.strelbova@gmail.com>
2023-02-25 09:54:29 +0100

    zakladni kresleni

--- src/main/java/cz/czechitas/turtle/HlavniProgram.java
+++ src/main/java/cz/czechitas/turtle/HlavniProgram.java
@@ -11,11 +11,46 @@ public class HlavniProgram {
         zofka = new Turtle();
 
 
+       /* cvicne nakreslene obrazce
+
+        nakresliRovnoramennyPravouhlyTrojuhelnik(800,100,100,Color.ORANGE);
         nakresliRovnostrannyTrojuhelnik(150,150,100, Color.cyan);
         nakresliCtvrec(300,300,100, Color.MAGENTA);
+        nakresliObdelnik(500, 500, 100, 200, Color.green);
+        nakresliKruh(500,100,1,Color.BLUE);
+
+        */
 
 
     }
+    private void nakresliRovnoramennyPravouhlyTrojuhelnik (double startX, double startY, double delkaStrany, Color barva)
+    { zofka.setPenColor(barva);
+        zofka.setLocation(startX,startY);
+      zofka.move(delkaStrany);
+        zofka.turnLeft(135);
+        var delkaPrepony = Math.sqrt(2*Math.pow(delkaStrany, 2));
+        zofka.move(delkaPrepony);
+        zofka.turnLeft(135);
+        zofka.move(delkaStrany);
+        zofka.turnLeft(90);}
+
+    private void nakresliKruh (double startX, double startY,double polomer,Color barva){
+        zofka.setPenColor(barva);
+        zofka.setLocation(startX,startY);
+        for (int i = 0; i < 360; i++) {
+            zofka.move(polomer);
+            zofka.turnLeft(1);
+        }
+    }
+    private void nakresliObdelnik(double startX, double startY, double delkaStranyA, double delkaStranyB, Color barva){
+        zofka.setPenColor(barva);
+        zofka.setLocation(startX,startY);
+        for (int i = 0; i < 2; i++) {
+            zofka.move(delkaStranyA);
+            zofka.turnRight(90);
+            zofka.move(delkaStranyB);
+            zofka.turnRight(90);
+        }}
     private void nakresliCtvrec(double startX, double startY, double delkaStrany, Color barva){
             zofka.setPenColor(barva);
             zofka.setLocation(startX,startY);
