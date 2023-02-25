934099b96daf0da77cf0e3e3a871a0f799252250
alzbetahladikova <beta.strelbova@gmail.com>
2023-02-25 09:33:18 +0100

    zakladni kresleni

--- src/main/java/cz/czechitas/turtle/HlavniProgram.java
+++ src/main/java/cz/czechitas/turtle/HlavniProgram.java
@@ -2,12 +2,35 @@ package cz.czechitas.turtle;
 
 import cz.czechitas.turtle.engine.*;
 
+import java.awt.*;
+
 public class HlavniProgram {
     private Turtle zofka;
 
     public void start() {
         zofka = new Turtle();
+
+
+        nakresliRovnostrannyTrojuhelnik(150,150,100, Color.cyan);
+        nakresliCtvrec(300,300,100, Color.MAGENTA);
+
+
     }
+    private void nakresliCtvrec(double startX, double startY, double delkaStrany, Color barva){
+            zofka.setPenColor(barva);
+            zofka.setLocation(startX,startY);
+        for (int i = 0; i < 4; i++) {
+               zofka.move(delkaStrany);
+               zofka.turnRight(90);}}
+
+    private void nakresliRovnostrannyTrojuhelnik(double startX,double startY,double delkaStrany, Color barva) {
+        zofka.setPenColor(barva);
+        zofka.setLocation(startX,startY);
+      for (int i =0; i<3;i++) {zofka.move(delkaStrany);
+        zofka.turnLeft(120);
+        }
+    }
+
 
     public static void main(String[] args) {
         new HlavniProgram().start();
